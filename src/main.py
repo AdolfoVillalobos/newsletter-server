c02377259c2e252c11dd7520d0585e099c53aa28
Adolfo Villalobos <amvillalobos@uc.cl>
2024-10-25 12:58:09 +1100

    add corsmiddleware and logger

--- src/main.py
+++ src/main.py
@@ -5,10 +5,11 @@ from typing import Annotated
 import pymongo
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, BeforeValidator, EmailStr, Field, HttpUrl, field_serializer
 from pydantic_core import Url
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('uvicorn')
 
 load_dotenv('.env')
 
@@ -21,19 +22,30 @@ class EmailSubscription(BaseModel):
     id: PyObjectId | None = Field(alias='_id', default=None)
     email: EmailStr = Field(..., description='The email address of the subscriber')
     name: str = Field(..., description='The name of the subscriber')
-    origin_url: HttpUrl | str = Field(..., description='The origin URL of the subscriber')
+    domain: HttpUrl | str = Field(..., description='The origin URL of the subscriber')
 
-    @field_serializer('origin_url')
-    def serialize_origin_url(self, val):
+    @field_serializer('domain')
+    def serialize_domain(self, val):
         if isinstance(val, Url):
             return str(val)
         return val
 
 
-app = FastAPI(title='Newsletter API', version='0.1.0')
+app = FastAPI(title='Newsletter API', version='0.1.0', debug=True)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+    allow_credentials=True,
+)
+
 client = pymongo.MongoClient(os.environ['MONGODB_URL'])
 db = client.newsletter
 subscriber_collection = db.get_collection('subscribers')
+
+logger.info('Connected to MongoDB')
 
 
 @app.get('/')
@@ -43,7 +55,7 @@ def read_root():
 
 
 @app.post(
-    '/subscribed/',
+    '/subscribe/',
     response_description='New subscriber',
     response_model=EmailSubscription,
     status_code=201,
@@ -51,6 +63,7 @@ def read_root():
 )
 def create_subscriber(subscriber: EmailSubscription):
     """Subscribe to the newsletter."""
+    logger.info('Creating subscriber: %s', subscriber)
     payload = subscriber.model_dump(by_alias=True, exclude=['id'])
     new_subscriber = subscriber_collection.insert_one(payload)
     created_subscriber = subscriber_collection.find_one({'_id': new_subscriber.inserted_id})
